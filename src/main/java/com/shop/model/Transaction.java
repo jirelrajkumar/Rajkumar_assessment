4489c37c43f77de6e0ea843505fe96f435ca61c2
Rajkumar <rajkumarjirel2015@gmail.com>
2023-01-05 11:39:12 -0600

    Initial commit

---
+++ src/main/java/com/shop/model/Transaction.java
@@ -0,0 +1,36 @@
+package com.shop.model;
+
+import java.util.Date;
+
+public class Transactions {
+
+    private String customerId;
+
+    private Double amount;
+
+    private Date date;
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ src/main/java/com/shop/pojo/CustomerRewardPojo.java
@@ -0,0 +1,4 @@
+package com.shop.pojo;
+
+public class RewardPojo {
+}
